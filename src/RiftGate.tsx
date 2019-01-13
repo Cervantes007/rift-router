9a7f90c1ec20e4a03a4f15158aa7a0217c6477cc
cervantes <lmartellguevara@gmail.com>
2019-01-12 20:33:14 -0500

    Improve RiftGate

--- src/RiftGate.tsx
+++ src/RiftGate.tsx
@@ -5,6 +5,10 @@ import { IRouter, RiftContext } from './RiftProvider';
 export const RiftGate = () => {
   const router = useContext<IRouter>(RiftContext);
   const index = router.register();
+  if (!router.active) {
+    console.error(`RiftGate nested level: ${index} have no active component`);
+    return null;
+  }
   const component = router.active.components[index];
   if (!component) {
     console.error(
